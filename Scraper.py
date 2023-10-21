737b581581f375f8e4e927e966b992cf666fd1e4
CarlosABrizuela <carlosalfredobrizuela@hotmail.com>
2023-10-21 09:13:04 -0300

    selenium package added, making a Get to a category from sucursal 1 and obtaining the json with products info

--- Scraper.py
+++ Scraper.py
@@ -1,7 +1,10 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 class Scraper:
     def __init__(self) -> None:
-        self.driver = None
+        self.driver = webdriver.Chrome()
 
     def create_csv(self):
         pass
@@ -12,5 +15,10 @@ class Scraper:
     def process_category(self):
         pass
 
-    def run(self):
-        print('main')
+    def run(self, url):
+        print('main runing...')
+        self.driver.get(url)
+        sleep(5)
+        script_element= self.driver.find_element(By.XPATH, '//script[@type="application/ld+json"]')
+        print(script_element.get_attribute("innerHTML"))
+
--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from Scraper import Scraper
 
 def main():
     scraper = Scraper()
-    scraper.run()
+    url = 'https://www.hiperlibertad.com.ar/tecnologia/tv-y-video/tv-led-y-smart-tv?sc=1'
+    scraper.run(url)
 
 if __name__ == "__main__":
     main()
