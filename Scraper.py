076504233a95407e590e2f73f3bbedcfaf306096
CarlosABrizuela <carlosalfredobrizuela@hotmail.com>
2023-10-22 16:22:38 -0300

    se añadió la función procesar producto, y find_elements

--- Scraper.py
+++ Scraper.py
@@ -12,8 +12,35 @@ class Scraper:
     def create_csv(self):
         pass
 
-    def process_product(self):
-        pass
+    def process_product(self, grid_productos, item_list_element):
+        try:
+            product_list = []
+            grid_items_productos= self.find_elements(grid_productos, By.XPATH, './div') # obtenemos cada elemento del grid
+            for item_element, grid_item_producto in zip(item_list_element, grid_items_productos): 
+                # iterar los elementos del grid y del json al mismo tiempo (el json no tiene el precio de lista o tachado)
+                producto= item_element['item']
+                dict_prod= {
+                'nombre': producto['name'],
+                'precio_publicado': producto['offers']['offers'][0]['price'],
+                'categoria': "CATEGORIA",
+                'SKU': producto['sku'],
+                'url_producto': producto['@id'],
+                'stock': producto['offers']['offers'][0]['availability'],
+                'descripcion': str(producto['description']).replace('\n', ' ').replace('\r', '') # quitamos los espacios
+                }
+                precio_regular= self.find_element(grid_item_producto, By.CLASS_NAME,'vtex-product-price-1-x-listPrice')
+                if precio_regular:
+                    dict_prod['precio_regular']= str(precio_regular.text).replace('$ ', '').replace('.', '').replace(',','.') # quitamos el '$ ' al precio
+                else:
+                    dict_prod['precio_regular']= producto['offers']['offers'][0]['price']
+
+                product_list.append(dict_prod)
+                
+        except Exception as e:
+            print(f'Hubo un error: {e}')
+        
+        # return product_list
+        pprint.pprint(product_list)
 
     def process_category(self):
         pass
@@ -23,33 +50,36 @@ class Scraper:
         self.driver.get(url)
         sleep(2)
         item_list_element= None
-        script_elements= self.driver.find_elements(By.XPATH, '//script[@type="application/ld+json"]')
+        script_elements= self.find_elements(self.driver, By.XPATH, '//script[@type="application/ld+json"]')
         for script_element in script_elements:
             dict_script_element = json.loads(script_element.get_attribute('innerHTML'))
             if dict_script_element['@type'] == "ItemList":
                 item_list_element= dict_script_element['itemListElement']
 
-        grid_productos= self.driver.find_element(By.ID, 'gallery-layout-container')
-        grid_items_productos= grid_productos.find_elements( By.XPATH, './div') # obtenemos cada elemento del grid
-        product_list = []
-        for item_element, grid_item_producto in zip(item_list_element, grid_items_productos): 
-            # iterar los elementos del grid y del json al mismo tiempo (el json no tiene el precio de lista o tachado)
-            producto= item_element['item']
-            dict_prod= {
-            'nombre': producto['name'],
-            'precio_publicado': producto['offers']['offers'][0]['price'],
-            'categoria': "CATEGORIA",
-            'SKU': producto['sku'],
-            'url_producto': producto['@id'],
-            'stock': producto['offers']['offers'][0]['availability'],
-            'descripcion': str(producto['description']).replace('\n', ' ').replace('\r', '') # quitamos los espacios
-            }
-            try:
-                precio_regular= grid_item_producto.find_element(By.CLASS_NAME,'vtex-product-price-1-x-listPrice')
-                dict_prod['precio_regular']= str(precio_regular.text).replace('$ ', '').replace('.', '').replace(',','.') # quitamos el '$ ' al precio
-            except NoSuchElementException:
-                dict_prod['precio_regular']= producto['offers']['offers'][0]['price']
+        grid_productos= self.find_element(self.driver,By.ID, 'gallery-layout-container')
 
-            product_list.append(dict_prod)
-        
-        pprint.pprint(f'Salida {product_list}')
+        self.process_product(grid_productos, item_list_element)
+    
+    def find_element(self, elemento,  by, value):
+        """parameters: 
+                elemento: puede ser driver o el elemento para buscar dentro de su html
+                by: By.CLASS, By.ID
+                value: nombre de la clase, nombre del id.
+        """
+        try:
+            return elemento.find_element(by, value) #elemento puede ser driver
+        except NoSuchElementException:
+            print(f"No se encontro el elemento:'{value}'")
+            return False
+    
+    def find_elements(self, elemento,  by, value):
+        """parameters: 
+                elemento: puede ser driver o el elemento para buscar dentro de su html
+                by: By.CLASS, By.ID
+                value: nombre de la clase, nombre del id.
+        """
+        try:
+            return elemento.find_elements(by, value) #elemento puede ser driver
+        except NoSuchElementException:
+            print(f"No se encontro el elemento: ({by})'{value}'")
+            return False
